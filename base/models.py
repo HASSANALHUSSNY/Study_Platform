c30865108d88a9b27c5aad858bd18415077730a0
HASSANALHUSSNY <hassan.alhussny@gmail.com>
2025-12-31 19:49:23 +0300

    Update

--- base/models.py
+++ base/models.py
@@ -18,6 +18,9 @@ class Room(models.Model):
     updated = models.DateTimeField(auto_now=True)
     Created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-Created']
+
     def __str__(self):
         return self.name
 
